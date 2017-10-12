6b203f23a4bf27adebb255da4e2b0f3ceeb918c5
Aleksey <agat00@gmail.com>
2017-10-12 13:59:30 +0300

    add traders to config

--- config/privateBittrix.js
+++ config/privateBittrix.js
@@ -1,9 +1,39 @@
 'use strict'
 
 exports.credentials = [{
-  name: 'first',
+  name: 'Vladimir',
   key: '5301d2f6e27643c8a504cc2ca0b2466a',
   secrets: {
     read: '2eb4851ece48418eb093ebcd862e0182'
+  }
+}, {
+  name: 'Alexey',
+  key: 'edaf40a54f1e465eaeab91ede87b6c9c',
+  secrets: {
+    read: '3cac610043144300967d4989897afeb3'
+  }
+}, {
+  name: 'Yaroslav',
+  key: 'd17b6c5dfbe24a5bb830cecd78783ad0',
+  secrets: {
+    read: '3517fa65bb5b4bae99ee4660fe18a206'
+  }
+}, {
+  name: 'Yuriy',
+  key: 'e6f4430d8b4240c792078730bd6d5b37',
+  secrets: {
+    read: '287df58db2574098bf04f0d6e66b237e'
+  }
+}, {
+  name: 'Mikhail',
+  key: '95a30c7e284943d792480518bd499506',
+  secrets: {
+    read: '417ae6bf36004c258d0fae184d48d36a'
+  }
+}, {
+  name: 'Alexey 1',
+  key: '6b4307f661f24b27aaad02b0a8fdcf0a',
+  secrets: {
+    read: '2cef47de025d412e8ba8e9ea35769140'
   }
 }]
