8ddb4d0e51ff0cb1d758a4e698344c14cbe08601
Aleksey Bobyr <aleksey.bobyr@beetroot.se>
2017-11-22 19:15:38 +0200

    auto commit

--- src/public/config.js
+++ src/public/config.js
@@ -6,8 +6,8 @@ const config = {
   bittrexCommission: 0.0025,
   useFakeMiners: true,
   fakeMiningTable: {
-    ETH:   {deposit: 12,    withdrawal: 0.8},
-    XMR:   {deposit: 3,     withdrawal: 0.4},
-    MUSIC: {deposit: 55000, withdrawal: 150},
+    ETH:   {deposit: 10,    withdrawal: 0.2},
+    XMR:   {deposit: 2,     withdrawal: 0.3},
+    MUSIC: {deposit: 53000, withdrawal: 78},
   }
 }
