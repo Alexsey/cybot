1aff7ca45f5d8eef7a0ae8ed909a2f3a43723a8b
tradethetape <trade.the.tape.ua@gmail.com>
2017-11-26 12:30:22 +0200

    auto commit

--- src/public/config.js
+++ src/public/config.js
@@ -6,8 +6,8 @@ const config = {
   bittrexCommission: 0.0025,
   useFakeMiners: true,
   fakeMiningTable: {
-    ETH:   {deposit: 40.091569585868,    withdrawal: 0},
+    ETH:   {deposit: 41.092493715868,    withdrawal: 0},
     XMR:   {deposit: 17.892112288329,     withdrawal: 0},
-    MUSIC: {deposit: 104754.15123035, withdrawal: 0},
+    MUSIC: {deposit: 107005.04376457, withdrawal: 0},
   }
 }
