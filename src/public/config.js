e6c5441945caa8a5aa6934141de384080ba2d31a
Alexsey <agat00@gmail.com>
2017-11-29 20:10:02 +0200

    bump packages & fix spacing

--- src/public/config.js
+++ src/public/config.js
@@ -6,8 +6,8 @@ const config = {
   bittrexCommission: 0.0025,
   useFakeMiners: true,
   fakeMiningTable: {
-    ETH:   {deposit: 42.092863715868,    withdrawal: 0},
-    XMR:   {deposit: 17.892112288329,     withdrawal: 0},
+    ETH:   {deposit: 42.092863715868, withdrawal: 0},
+    XMR:   {deposit: 17.892112288329, withdrawal: 0},
     MUSIC: {deposit: 113250.23108865, withdrawal: 0},
   }
 }
--- src/public/minersTable.js
+++ src/public/minersTable.js
@@ -135,5 +135,4 @@ function formMinersTableDataFake (rates) {
       totalUSDT
     }
   })
-
 }
