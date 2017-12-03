8a100018b894ba7c8df48d0a220b642ab85c2c25
Alexsey <agat00@gmail.com>
2017-12-04 00:15:15 +0200

    improve deployScheduler

--- src/deployScheduler.js
+++ src/deployScheduler.js
@@ -4,17 +4,18 @@ const _ = require('lodash')
 const moment = require('moment')
 const shell = require('shelljs')
 
-const [hours, minutes, seconds] = process.argv[2].split(':')
-const time = moment().hours(hours).minutes(minutes || 0).seconds(seconds || 0).millisecond(0)
+const day = 24 * 60 * 60 * 1000
 
-if (time.isBefore(Date.now())) time.add(1, 'day')
+const [hours, minutes, seconds] = process.argv[2].split(':')
+let time = new Date().setHours(hours, minutes || 0, seconds || 0, 0)
+if (time <= Date.now()) time += day
 
 const gitStatus = shell.exec('git status')
 if (!gitStatus.stdout.includes('nothing to commit, working tree clean')) {
   shell.exec('git commit -am "auto commit" && git pull --rebase && git push')
 }
 
-console.log(`will deploy in ${moment.duration(time - Date.now()).humanize()}`)
+console.log(`\nWill deploy in ${moment.duration(time - Date.now()).humanize()}`)
 
 setTimeout(() => {
   shell.exec('git pull --rebase && npm run deploy')
