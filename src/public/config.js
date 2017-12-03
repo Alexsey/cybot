34cd3cfd6fa08435a42842c1f5792781bd360c0f
Alexsey <agat00@gmail.com>
2017-12-04 00:17:47 +0200

    auto commit

--- src/public/config.js
+++ src/public/config.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const config = {
-  periodStartDate: 12,
+  periodStartDate: 1,
   bittrexCommission: 0.0025,
   tradersTable: {
     // in hours. Could be float ex. 1/3. Set 0 to use actual time
