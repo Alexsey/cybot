a6236c31d9bbf208fadf1a4d967b0b74be3ac0ac
tradethetape <trade.the.tape.ua@gmail.com>
2018-01-10 14:32:40 +0200

    auto commit

--- src/public/config.js
+++ src/public/config.js
@@ -34,7 +34,7 @@ const config = {
     fakeData: {
       ETH:   {deposit: 14.31502010, withdrawal: 0},
       XMR:   {deposit: 17.892112288329, withdrawal: 0},
-      MUSIC: {deposit: 162062.69225606, withdrawal: 0},
+      MUSIC: {deposit: 163971.70273403, withdrawal: 0},
       SC:    {deposit: 500000, withdrawal: 0},
       BCN:   {deposit: 1033537.73850005, withdrawal: 0},
       RPX:   {deposit: 14598.1054818, withdrawal: 0},
