3caed011fe13991e36c6c63a839df8797a3127ae
tradethetape <trade.the.tape.ua@gmail.com>
2018-01-29 11:33:34 +0200

    auto commit

--- src/public/config.js
+++ src/public/config.js
@@ -32,8 +32,8 @@ const config = {
     currencies: ['ETH', 'XMR', 'MUSIC'],
     useFakeData: true,
     fakeData: {
-      ETC:   {deposit: 38.65999737, withdrawal: 0},
-      MUSIC: {deposit: 184566.07673240, withdrawal: 0},
+      ETC:   {deposit: 68.15841178, withdrawal: 0},
+      MUSIC: {deposit: 192090.45581365, withdrawal: 0},
       SC:    {deposit: 721797.19044156, withdrawal: 0},
       RCN:   {deposit: 13254.84479378, withdrawal: 0},
       BCN:   {deposit: 1033537.73850005, withdrawal: 0},
