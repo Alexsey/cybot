d0ead12bf9271e665c8db1d025fc64c4f93c2235
tradethetape <trade.the.tape.ua@gmail.com>
2018-03-07 16:52:50 +0200

    auto commit

--- config/privateBittrex.js
+++ config/privateBittrex.js
@@ -1,13 +1,7 @@
 'use strict'
 
 exports.credentials = [{
-  name: 'Vladimir',
-  roles: ['miner'],
-  key: '5301d2f6e27643c8a504cc2ca0b2466a',
-  secrets: {
-    read: '2eb4851ece48418eb093ebcd862e0182'
-  }
-}, {
+
   name: 'Main Account',
   roles: ['trader'],
   key: 'b68208d678e141ba8cb3f05d4a9c5c64',
