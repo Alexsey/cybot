3029ff1c8952fecb9d0655a7419e15200bd300b9
tradethetape <trade.the.tape.ua@gmail.com>
2018-03-30 16:43:15 +0300

    auto commit

--- config/privateBittrex.js
+++ config/privateBittrex.js
@@ -13,5 +13,11 @@ exports.credentials = [{
   key: '5301d2f6e27643c8a504cc2ca0b2466a',
   secrets: {
     read: '2eb4851ece48418eb093ebcd862e0182'
+}, {
+  name: 'Alex Account',
+  roles: ['trader'],
+  key: 'a99a1a83053b4074b05470b945e18b82',
+  secrets: {
+    read: '6db9f6c650cf4dd9a2a0eedc9afdf693'
   }
 }]
